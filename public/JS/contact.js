e87da6e97c7f6a65252b0f66a2512c5ad25b3ff1
Udu Jonaka Nnanna <udunnanna0@gmail.com>
2025-10-27 23:04:54 +0100

    contact navbar added

--- public/JS/contact.js
+++ public/JS/contact.js
@@ -20,6 +20,18 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     });
 });
 
+  // Toggle mobile menu
+  function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    var toggle = document.getElementById("nav-toggle");
+    navLinks.classList.toggle("active");
+    toggle.classList.toggle("active");
+  }
+
+  document
+    .getElementById("nav-toggle")
+    .addEventListener("click", toggleMenu);
+
 // Toggle light/dark mode
 const toogleBtn = document.getElementById('theme-toogle');
 
