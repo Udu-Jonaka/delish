42830c58aa422026a0ac67792a1afcff202aaea8
Udu Jonaka Nnanna <udunnanna0@gmail.com>
2025-11-19 18:54:08 +0100

    write media query and change pkg type

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 5100;
